eced39a8492860c7353052bf8f85d58224b88d6f
azizgadacha <aziz.gadacha@esprit.tn>
2024-05-10 13:53:50 +0100

    first

---
+++ pideployement/pideployement/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pideployement project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pideployement.settings')
+
+application = get_wsgi_application()
