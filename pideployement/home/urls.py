e9930fca4f485118b5c753622e5c3df284b1970c
azizgadacha <aziz.gadacha@esprit.tn>
2024-05-11 01:32:05 +0100

    fixing main

--- pideployement/home/urls.py
+++ pideployement/home/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('home.urls')),
+    path('',views.index,name="index"),
 
-    path("auth/", include("django.contrib.auth.urls")),
 
 ]
--- pideployement/pideployement/settings.py
+++ pideployement/pideployement/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS= [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/auth/login/'
 
-
+LOGOUT_REDIRECT_URL="/auth/logout"
 LOGIN_REDIRECT_URL="/"
